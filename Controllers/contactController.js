d9d90ebb403f219fdd849ef267bf561d264c8aa1
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-09-06 18:37:52 +0530

    crud updated

--- Controllers/contactController.js
+++ Controllers/contactController.js
@@ -3,52 +3,60 @@ import ContactSchema from "../model/contactmodel.js";
 
 const getContact = expressAsyncHandler(async (req, res) => {
   const contacts = await ContactSchema.findById(req.params.id);
-  if(!contacts){
+  if (!contacts) {
     res.status(404);
     throw new Error("Contact Not Found");
   }
-  res.json({messgage:`Fetched details from ${req.params.id}`,contacts});
-  
+  res.json({ messgage: `Fetched details from ${req.params.id}`, contacts });
+
 });
 
 const createContact = expressAsyncHandler(async (req, res) => {
   const { name, email, ph } = req.body;
 
   if (!name || !email || !ph) {
-    
-    res.status(400).json({ message: "Please provide name, email, and ph." });
-  } else {
-    
-   const createNewContact  = await ContactSchema.create({
-    name,email,ph
-   })
 
-    res.status(201).json(createNewContact); 
+    res.status(400).json({ message: "Please provide name, email, and ph." });
   }
+
+  const createNewContact = await ContactSchema.create({
+    name, email, ph
+  })
+
+  res.status(201).json(createNewContact);
+
 });
 
 const updateContact = expressAsyncHandler(async (req, res) => {
-  try {
-    const contact = await ContactSchema.findById(req.params.id);
-    if (!contact) {
-      res.status(404).json({ message: "Contact Not Found" });
-    } else {
-      const updatedContact = await ContactSchema.findByIdAndUpdate(
-        req.params.id,
-        req.body,
-        { new: true }
-      );
-      res.json({ message: `Updated contact with ID ${req.params.id}`, updatedContact });
-    }
-  } catch (error) {
-    res.status(500).json({ message: "Internal Server Error" });
+  const contact = await ContactSchema.findById(req.params.id);
+  if(!contact ){
+    res.status(400).json({message:"Plz update anything !"});
   }
+
+  const updateTheContact = await ContactSchema.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {new: true}
+  )
+  res.status(200).json(updateTheContact)
 });
 
 const deleteContact = expressAsyncHandler(async (req, res) => {
   const contactId = req.params.id;
 
-  res.json({ message: `Deleted contact with ID ${contactId}` });
+  try {
+    const result = await ContactSchema.deleteOne({ _id: contactId });
+
+    if (result.deletedCount === 0) {
+      res.status(404).json({ message: "Contact Not Found" });
+    } else {
+      res.json({ message: `Deleted contact with ID ${contactId}`, contactId });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error deleting contact" });
+  }
 });
+
 
 export { getContact, createContact, updateContact, deleteContact };
