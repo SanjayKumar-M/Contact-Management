e1d7201207d25f095eed50abbb31aaec03d892a9
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-09-06 12:46:23 +0530

    updated !

--- Controllers/contactController.js
+++ Controllers/contactController.js
@@ -2,8 +2,13 @@ import expressAsyncHandler from "express-async-handler";
 import ContactSchema from "../model/contactmodel.js";
 
 const getContact = expressAsyncHandler(async (req, res) => {
-  const contacts = await ContactSchema.find()
-  res.json(contacts);
+  const contacts = await ContactSchema.findById(req.params.id);
+  if(!contacts){
+    res.status(404);
+    throw new Error("Contact Not Found");
+  }
+  res.json({messgage:`Fetched details from ${req.params.id}`,contacts});
+  
 });
 
 const createContact = expressAsyncHandler(async (req, res) => {
@@ -23,9 +28,21 @@ const createContact = expressAsyncHandler(async (req, res) => {
 });
 
 const updateContact = expressAsyncHandler(async (req, res) => {
-  const contactId = req.params.id;
- 
-  res.json({ message: `Updated contact with ID ${contactId}` });
+  try {
+    const contact = await ContactSchema.findById(req.params.id);
+    if (!contact) {
+      res.status(404).json({ message: "Contact Not Found" });
+    } else {
+      const updatedContact = await ContactSchema.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+      );
+      res.json({ message: `Updated contact with ID ${req.params.id}`, updatedContact });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 const deleteContact = expressAsyncHandler(async (req, res) => {
